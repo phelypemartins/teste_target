5736f8207c51af62c09b9dff01dc30b157cd1c11
Felipe Martins <phelypemartins@gmail.com>
2024-09-02 22:00:54 -0300

    Alteração código exercício 5

--- exercicio_5/5.py
+++ exercicio_5/5.py
@@ -1,10 +1,3 @@
-def inverter_string(s):
-    invertida = ""
-    for char in s:
-        invertida = char + invertida
-    return invertida
-
-# Exemplo de uso
-string_teste = input('Digite uma palavra: ')
-string_invertida = inverter_string(string_teste)
-print(string_invertida)
+string = input('Digite uma palavra: ')
+invertida = string[::-1]
+print(invertida)
